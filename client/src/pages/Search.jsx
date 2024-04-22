4a605471d3bafa85fb82c22d97283cc05fc1846d
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-04-22 18:28:59 +0530

    create the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
     const navigate = useNavigate();
@@ -175,8 +176,19 @@ export default function Search() {
                 <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Search</button>
             </form>
         </div>
-        <div>
+        <div className='flex-1'>
             <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing Results:</h1>
+            <div className='p-7 flex flex-wrap gap-4'>
+                {!loading && listings.length === 0 && (
+                    <p className='text-xl text-slate-700'>No listing found!</p>
+                )}
+                {loading && (
+                    <p className='text-xl text-slate-700 text-center w-full'>Loading...</p>
+                )}
+                {!loading && listings && listings.map((listing) => (
+                    <ListingItem key={listing._id} listing={listing} />
+                ))}
+            </div>
         </div>
     </div>
   )
--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -227,7 +227,7 @@ export default function UpdateListing() {
                     </div>
                 ))}
                 <button disabled={loading || uploading} className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-                    {loading ? 'Creating...' : 'Update Listing'}
+                    {loading ? 'Updating...' : 'Update Listing'}
                 </button>
                 {error && <p className='text-red-700 text-sm'>{error}</p>}
             </div>
