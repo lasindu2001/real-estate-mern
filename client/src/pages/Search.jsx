9056abb286f4b61c3aa556cc1065699e6a3058fa
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-04-22 19:33:59 +0530

    add show more listings functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -15,7 +15,7 @@ export default function Search() {
     });
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
-    console.log(listings);
+    const [showMore, setShowMore] = useState(false);
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
         const searchTermFromUrl = urlParams.get('searchTerm');
@@ -46,9 +46,15 @@ export default function Search() {
         }
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if (data.length > 8) {
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         };
@@ -85,6 +91,19 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+    };
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
     };
   return (
     <div className='flex flex-col md:flex-row'>
@@ -188,6 +207,11 @@ export default function Search() {
                 {!loading && listings && listings.map((listing) => (
                     <ListingItem key={listing._id} listing={listing} />
                 ))}
+                {showMore && (
+                    <button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7 text-center w-full'>
+                        Show more
+                    </button>
+                )}
             </div>
         </div>
     </div>
