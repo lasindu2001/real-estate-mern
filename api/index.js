0d46e1ad8b87caec129aa7a371bab6840f67baa7
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-04-23 19:53:35 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
@@ -14,7 +15,10 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err);
 })
 
+const __dirname = path.resolve();
+
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,6 +29,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500
